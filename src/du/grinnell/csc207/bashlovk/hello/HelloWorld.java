5b747a6212081f95337800297916d9a9b00bb4a3
VBashlovkina <bashlovk@grinnell.edu>
2014-01-23 12:57:17 -0600

    mischief

--- src/du/grinnell/csc207/bashlovk/hello/HelloWorld.java
+++ src/du/grinnell/csc207/bashlovk/hello/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	 public static void 
 	    main (String[] args) 
 	  {
-	    System.out.println ("Hello, GitHub!");
+	    System.out.println ("What's up, GitHub?");
 	  } // main(String[])
 }
